057f3f254a58685acd61a42800eee310bdb1de43
Steven Moder <java20131114@gmail.com>
2024-05-22 16:32:33 +0800

    feat: Add user login functionality

--- src/main/java/com/sakurapuare/taskmanager_se/Controller/UserController.java
+++ src/main/java/com/sakurapuare/taskmanager_se/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.sakurapuare.taskmanager_se.Controller;
 
 import com.sakurapuare.taskmanager_se.POJO.DTO.UserDTO;
 import com.sakurapuare.taskmanager_se.common.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +14,14 @@ import java.util.ArrayList;
 
 @RestController
 @Slf4j
+@Tag(name = "用户管理")
 @RequestMapping("/user")
 public class UserController {
 
     ArrayList<UserDTO> users = new ArrayList<>();
 
     @PostMapping("/login")
+    @Operation(summary = "用户登录")
     public Result<String> login(@RequestBody UserDTO userDTO) {
         for (UserDTO user : users) {
             if (user.getUsername().equals(userDTO.getUsername()) && user.getPassword().equals(userDTO.getPassword())) {
